c4000dcc3ab4a60cd3171201946e1da4592e9ce4
fahimnirjon <fahimnirjon21@gmail.com>
2024-07-01 23:10:47 +0600

    footer changed

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,8 +1,28 @@
 const Category = () => {
     return (
-        <div className="text-center">
-            <p className="text-5xl ">Job Category List</p>
-            <p className="text-2xl">Explore thousands of job opportunities with all infos you need. Its your Future</p>
+        <div className="text-center mt-10 mb-20">
+            <div>
+            <p className="text-3xl text-purple-400">Choose Your Favorite Language & FrameWorks </p>
+            <p className="text-2xl font-light mt-5">Explore thousands of opportunities with all infos you need. Its your Future</p>
+            </div>
+           <div>
+           <select className="select select-primary w-full max-w-xs mt-4">
+                <option selected>What is the Best Language?</option>
+                <option>C++</option>
+                <option>Java</option>
+                <option>PyThon</option>
+                <option>JavaScript</option>
+            </select>
+            <select className="select select-bordered w-full max-w-xs mt-5">
+                <option selected>What Is The Best FrameWork?</option>
+                <option>React</option>
+                <option>Django</option>
+                <option>Flask</option>
+                <option>Laravel</option>
+                <option>Ruby</option>
+                <option>Vue.Js</option>
+            </select>
+           </div>
         </div>
     );
 };
